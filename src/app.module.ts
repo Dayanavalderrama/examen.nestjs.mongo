9f730d00f4c6d14880c4d2ccd37977b61a5c7cc6
Dayana Artunduaga <Dayanaartunduaga@gmail.com>
2026-05-06 21:27:52 -0500

    segundo avance

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,3 +8,18 @@ import { AppService } from './app.service';
   providers: [AppService],
 })
 export class AppModule {}
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { CategoriasModule } from './categorias/categorias.module';
+import { ProductosModule } from './productos/productos.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_URI || 'mongodb://localhost:27017/catalogo-api'),
+    CategoriasModule,
+    ProductosModule,
+  ],
+})
+export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  // Si tienes un prefijo global, coméntalo por ahora para probar
+  // app.setGlobalPrefix('api'); 
+  await app.listen(3000);
+  console.log("Servidor corriendo en: http://localhost:3000");
 }
 bootstrap();
